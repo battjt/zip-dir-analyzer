918f4655a4c429bce0aa90310917b3e709ded942
Joe Batt <Joe@SolidDesign.net>
2025-01-18 16:08:07 -0500

    "works"

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::{
     fs::{self, File},
-    io::{self, BufRead, Read, Seek, Write},
+    io::{self, BufRead, Read, Write},
     path::Path,
     sync::{Arc, Mutex},
 };
 
+use acc_reader::AccReader;
 use anyhow::Result;
 use clap::Parser;
 use executors::threadpool_executor::ThreadPoolExecutor;
@@ -13,6 +14,7 @@ use regex::Regex;
 fn main() -> Result<()> {
     Logga::run(Cli::parse())
 }
+
 #[derive(Parser, Debug, Default, Clone)]
 pub struct Cli {
     #[arg()]
@@ -25,13 +27,13 @@ pub struct Cli {
     report_file: Option<String>,
 }
 
-#[derive(Clone)]
 struct Logga<W: Write> {
     args: Cli,
     pool: ThreadPoolExecutor,
     out: Arc<Mutex<W>>,
     rx: Regex,
 }
+
 impl Logga<File> {
     fn run(args: Cli) -> Result<()> {
         let root_dir = args.root_dir.clone();
@@ -47,60 +49,49 @@ impl Logga<File> {
             out,
             rx,
         }
-        .run_path(Path::new(root_dir.as_str()))
+        .walk_path(Path::new(root_dir.as_str()))
     }
 }
 impl<W: Write> Logga<W> {
-    /*
-    1. dir
-    2. zip file
-         file within zip
-         zip within zip
-    3. file
-    */
-    fn run_path(&self, path: &Path) -> Result<()> {
+    fn walk_path(&self, path: &Path) -> Result<()> {
         let path_str = path.to_str().unwrap();
         if path.is_dir() {
-            self.run_dir(&path)
+            self.walk_dir(&path)
         } else if path_str.ends_with(".zip") {
-            self.run_zip(path_str, fs::File::open(path)?)
+            let mut file = fs::File::open(path)?;
+            self.walk_zip(path_str, &mut file)
         } else {
-            self.grep(path_str, &File::open(path)?)
+            self.grep_file(path_str, &File::open(path)?)
         }
     }
 
-    fn run_zip<T: Read + Seek>(&self, path: &str, zip_file: T) -> Result<(), anyhow::Error> {
-        let mut archive = zip::ZipArchive::new(zip_file)?;
+    fn walk_zip(&self, path: &str, zip_file: &mut dyn Read) -> Result<(), anyhow::Error> {
+        let mut archive = zip::ZipArchive::new(AccReader::new(zip_file))?;
         for i in 0..archive.len() {
-            let (file_name, is_dir) = {
-                let zf = archive.by_index(i)?;
-                (zf.name().to_string(), zf.is_dir())
-            };
-            if is_dir {
+            let mut zip_file = archive.by_index(i)?;
+            if zip_file.is_dir() {
                 // just a directory placeholder.
             } else {
-                let file_name = path.to_string() + "!" + &file_name;
-                let zip_seek = archive.by_index_seek(i)?;
+                let file_name = path.to_string() + "!" + &zip_file.name().to_string();
                 if file_name.ends_with(".zip") {
-                    self.report(&file_name, "zip in zip not supported")?;
-                    self.run_zip(&file_name, Box::new(zip_seek))?;
+                    self.walk_zip(&file_name, &mut zip_file)?;
                 } else {
-                    self.grep(&file_name, zip_seek)?;
+                    self.grep_file(&file_name, zip_file)?;
                 }
             }
         }
         Ok(())
     }
 
-    fn run_dir(&self, path: &Path) -> Result<(), anyhow::Error> {
+    fn walk_dir(&self, path: &Path) -> Result<(), anyhow::Error> {
         for d in std::fs::read_dir(path)? {
             let path_buf = d?.path();
-            self.run_path(path_buf.as_path())?;
+            self.walk_path(path_buf.as_path())?;
         }
         Ok(())
     }
 
-    fn grep<T: Read>(&self, path: &str, data: T) -> Result<(), anyhow::Error> {
+    fn grep_file<T: Read>(&self, path: &str, data: T) -> Result<(), anyhow::Error> {
         let lines = io::BufReader::new(data).lines();
         for line in lines {
             let line = line?;
